bec543e266bbe10f963eaaf705ee47178d4394ef
Jesse Wenger <wengerj@gmail.com>
2021-05-18 21:13:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+let mn = document.querySelector("main#main");
+mn.remove()
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = 'JESSE WENGER is the champion'
